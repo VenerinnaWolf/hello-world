1f9ff1b029b96c8f9a82d3872f1b759cffd13580
Veronika <101918389+VenerinnaWolf@users.noreply.github.com>
2025-04-07 18:42:04 +0300

    Added comments

--- Scala/src/main/scala/playground/HelloWorld.scala
+++ Scala/src/main/scala/playground/HelloWorld.scala
@@ -1,20 +1,21 @@
 package playground
 
-// Два варианта написания кода для запуска https://stepik.org/lesson/460681/step/4?auth=registration&unit=451276
+// Два варианта написания кода для запуска из курса Stepic https://stepik.org/lesson/460681/step/4?auth=registration&unit=451276
 
 // Вариант 1 (в версии Scala 3. советуют не использовать)
 object ScalaPlaygroundOne extends App {
   println("Hello, Scala, from Stepic object ScalaPlaygroundOne!")
 }
 
-// Вариант 2
+// Вариант 2 (такой вариант можно использовать, чтобы работало одновременно для Scala 2 и 3)
 object ScalaPlaygroundTwo {
   def main(args: Array[String]) : Unit = {
     println("Hello, Scala, from Stepic object ScalaPlaygroundTwo main function!")
   }
 }
 
-
+// Для Scala 3 лучше использовать вот такую запись
 // Hello world более новой версии: https://docs.scala-lang.org/ru/getting-started/intellij-track/building-a-scala-project-with-intellij-and-sbt.html
-@main def run(): Unit =
+@main def run(): Unit = {
   println("Hello, Scala, from newer documentation!")
+}
