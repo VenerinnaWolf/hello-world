17e427cb9f1ddcd501095a2ebab6530fe1458876
Veronika <101918389+VenerinnaWolf@users.noreply.github.com>
2025-04-14 15:00:20 +0300

    Changed file structure in Python directory

--- Python/Helloworld.py
+++ Python/Helloworld.py
@@ -1,1 +1,29 @@
+import os
+from pathlib import Path
+
 print("Hello, world!")
+print(os.system('tree'))
+
+
+fork_string   = '├──'
+corner_string = '└──'
+wall_string   = '│  '
+space_string  = '   '
+
+def drawtree(path, seen, head='', tail=''):
+    if path.is_dir() and path.resolve() not in seen:
+        yield head + path.name
+        seen.add(path.resolve()) # на случай зацикленных ссылок
+        entries = sorted(filter(Path.is_dir, path.iterdir()))
+
+        for i, entry in enumerate(entries):
+            if i < len(entries) - 1:
+                yield from drawtree(entry, seen, tail + fork_string, tail + wall_string)
+            else:
+                yield from drawtree(entry, seen, tail + corner_string, tail + space_string)
+
+def tree(my_path='.'):
+    for line in drawtree(Path(my_path), set()):
+        print(line)
+
+tree('root')
